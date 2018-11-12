f72b5ee70c8f6e85833851a1d2978c60dc769acf
YoonjuSeo <yoonjuseo08@gmail.com>
2018-11-11 22:14:53 -0800

    20181111

---
+++ src/Yoonju.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Yoonju from './Yoonju'
+
+class Yoonju extends Component{
+    render(){
+        return(
+            <div className = "yoonju">
+                <div>Name : Clover</div>
+                <div>Age: 2</div>
+                <div>Fur: White</div>
+            </div>
+        )
+    }
+}
+
+export default Yoonju
