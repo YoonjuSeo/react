755002e5317336568b917759b9840d215a7b8194
YoonjuSeo <yoonjuseo08@gmail.com>
2018-11-12 23:06:04 -0800

    20181112

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import Yoonju from './Yoonju'
 
 class App extends Component {
+  state = {
+    info : [
+      {name: 'Soma', age:'3', color:'brown', id:1},
+      {name: 'Clover', age:'2', color:'white', id:2},
+      {name: 'Chance', age:'1', color:'brown', id:3}
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>My first react app!</h1>
         <p>Welcome</p>
-        <Yoonju name="Soma" age="3" fur="brown"/>
-        <Yoonju name="Clover" age = "2" fur="white"/>
+        <Yoonju info={this.state.info}/>
       </div>
     );
   }
--- src/Yoonju.js
+++ src/Yoonju.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 
-class Yoonju extends Component{
-    render(){
-        //console.log(this.props);
-        const{name,age,fur}= this.props;
-        return(
-            <div className = "yoonju">
-                <div>Name : {name}</div>
-                <div>Age: {age}</div>
-                <div>Fur: {fur}</div>
-            </div>
+const Yoonju =(props)=>{
+    //console.log(this.props);
+    const{info}= props;
+    const infoList = info.map(item => {
+        return (
+        <div className = "yoonju" key={item.id}> {/* key has to be unique */}
+            <div>Name : {item.name}</div>
+            <div>Age: {item.age}</div>
+            <div>Color: {item.color}</div>
+        </div>
         )
-    }
+    })
+    return(
+        <div className="info-list">
+            {infoList}
+        </div>
+    )
 }
 
 export default Yoonju
