54623c449dcac2575ac03886648bc3001128b673
YoonjuSeo <yoonjuseo08@gmail.com>
2018-11-24 21:06:05 -0800

    lesson23 181124

--- src/App.js
+++ src/App.js
@@ -26,6 +26,13 @@ class App extends Component {
       info: info
     });
   }
+  componentDidMount(){
+    console.log('component mounted');
+  }
+  componentDidUpdate(prevProps, prevState){
+    console.log('component updated');
+    console.log(prevProps,prevState);
+  }
   render() {
     return (
       <div className="App">
